0b96ec239ff6689ef6fe9fb296fe85dd23b4035b
Sai Gonuguntla <75554919+saigonu@users.noreply.github.com>
2024-04-28 01:48:13 -0700

    changes

--- routes/post_object.py
+++ routes/post_object.py
@@ -9,3 +9,14 @@ def overview():
     if exists== False :
         flash(f'Add products,locations and make transfers to view','info')
     return render_template('overview.html' ,balance=balance)
+
+
+@app.route("/Product", methods = ['GET','POST'])
+def product():
+    form = addproduct()
+    eform = editproduct()
+    details = Product.query.all()
+    exists = bool(Product.query.all())
+    if exists== False and request.method == 'GET' :
+            flash(f'Add products to view','info')
+    elif eform.validate_on_submit() and request.method == 'POST':
